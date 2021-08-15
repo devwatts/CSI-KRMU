dfb7c13ddf347d267867b9c20771810d44026ea6
Satya Shekhar <satyashekhar064@gmail.com>
2021-08-15 15:48:05 +0530

    changes

---
+++ index.js
@@ -0,0 +1,44 @@
+$('.owl-1').owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+$('.owl-2').owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+
+    responsive:{
+        0:{
+            items:4
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:4
+        }
+    }
+})
